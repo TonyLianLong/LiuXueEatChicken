e6588b94c92e2dbcebe30d9bda630139229654bc
Tony Lian <1040424979@qq.com>
2018-01-26 09:00:31 +0800

    Fix a typo and relocate the deploy_key

--- deploy.sh
+++ deploy.sh
@@ -28,7 +28,7 @@ git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
 
 # Clean out existing contents
-rm -rf dost/**/* || exit 0
+rm -rf dist/**/* || exit 0
 
 # Run our compile script
 doCompile
@@ -57,7 +57,7 @@ ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out ../deploy_key -d
 chmod 600 ../deploy_key
 eval `ssh-agent -s`
-ssh-add deploy_key
+ssh-add ../deploy_key
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
