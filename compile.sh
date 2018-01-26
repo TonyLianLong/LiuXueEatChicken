a57dc36e1d3f47c5f9f7c951ab2a87ea4b4bb53c
Tony Lian <1040424979@qq.com>
2018-01-26 22:35:54 +0800

    Move compile.sh into deploy.sh and .travis.yml

--- compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e # Exit with nonzero exit code if anything fails
-rm yarn.lock # this is because taobao registry sometimes causes timeout and we don't really need specific version of packages
-yarn install
-yarn run build
