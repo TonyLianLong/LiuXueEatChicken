08c95bfa48f35a7c6cad3ad1c0c455c0e526c032
Tony Lian <1040424979@qq.com>
2018-01-31 15:44:38 +0800

    Add wx into globals

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -15,7 +15,10 @@ const createLintingRule = () => ({
   include: [resolve('src'), resolve('test')],
   options: {
     formatter: require('eslint-friendly-formatter'),
-    emitWarning: !config.dev.showEslintErrorsInOverlay
+    emitWarning: !config.dev.showEslintErrorsInOverlay,
+    globals: [
+      'wx'
+    ]
   }
 })
 
@@ -59,7 +62,7 @@ module.exports = {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
         options: {
-          limit: 10000,
+          limit: 1000,
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
         }
       },
@@ -67,7 +70,7 @@ module.exports = {
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
         loader: 'url-loader',
         options: {
-          limit: 10000,
+          limit: 1000,
           name: utils.assetsPath('media/[name].[hash:7].[ext]')
         }
       },
@@ -75,7 +78,7 @@ module.exports = {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         loader: 'url-loader',
         options: {
-          limit: 10000,
+          limit: 1000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
       }
