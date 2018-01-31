576caf8568aecc78ab6d621691fac492625eb9f5
Tony Lian <1040424979@qq.com>
2018-01-31 15:44:59 +0800

    Add shared function
    Requires wx.config in index.html

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import logo from 'assets/logo.jpg'
 
 Vue.config.productionTip = false
 
@@ -12,4 +13,47 @@ new Vue({
   router,
   components: { App },
   template: '<App/>'
+})
+function wxProcessLink (link) {
+  if (link.indexOf('/') === 0) {
+    return 'http://xmoclxcj.duapp.com' + link
+  }
+}
+wx.ready(function () {
+  wx.onMenuShareAppMessage({
+    title: '留学吃鸡小游戏',
+    desc: '留学圈最火的一个知识竞赛小游戏',
+    link: wxProcessLink('/'),
+    imgUrl: wxProcessLink(logo),
+    trigger: function (res) {
+      // alert('用户点击发送给朋友');
+    },
+    success: function (res) {
+      // alert('已分享');
+    },
+    cancel: function (res) {
+      // alert('已取消');
+    },
+    fail: function (res) {
+      // alert(JSON.stringify(res));
+    }
+  })
+
+  wx.onMenuShareTimeline({
+    title: '留学吃鸡小游戏',
+    link: wxProcessLink('/'),
+    imgUrl: wxProcessLink(logo),
+    trigger: function (res) {
+      // alert('用户点击分享到朋友圈');
+    },
+    success: function (res) {
+      // alert('已分享');
+    },
+    cancel: function (res) {
+      // alert('已取消');
+    },
+    fail: function (res) {
+      // alert(JSON.stringify(res));
+    }
+  })
 })
