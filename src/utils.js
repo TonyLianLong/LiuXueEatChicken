66fc8d93084efa4435d98c9be9368b8ace69cc09
Tony Lian <1040424979@qq.com>
2018-02-11 11:11:05 +0800

    Add getFullLevel

--- src/utils.js
+++ src/utils.js
@@ -11,6 +11,17 @@ export default {
       .then(success)
       .catch(fail)
   },
+  getFullLevel (params, success = function (response) {
+    console.log(response)
+  }, fail = function (error) {
+    console.log(error)
+    alert('获取排行榜出错了 ' + error)
+  }) {
+    console.log('Sending getFullLevel request', params)
+    return axios.post('https://server2.tonylian.com/fullleveltable.php', params)
+      .then(success)
+      .catch(fail)
+  },
   getUrlKey (name) {
     // eslint-disable-next-line
     return decodeURIComponent((new RegExp('[?|&]'+name+'='+'([^&;]+?)(&|#|;|$)').exec(location.href)||[,""])[1].replace(/\+/g,'%20'))||null
